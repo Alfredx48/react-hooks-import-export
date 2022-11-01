f644ae8a5f0e9d34bf83e4ca6c5a5d7cad35306c
Alfred Shaheen <shaheenalfred48@gmail.com>
2022-10-31 20:04:57 -0700

    commit tree f7dbb9abe3278ca9eb424cc4bc694d57c40fd809

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,38 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import { trees, elevation, wildlife } from "./parks/RockyMountain";
+import * as RMFunctions from "./parks/RockyMountain"
+import MesaVerde from "./parks/MesaVerde"
 
-function ColoradoStateParks() {
+console.log(RMFunctions.trees)
+
+RMFunctions.wildlife()
+
+RMFunctions.elevation()
+
+console.log(trees);
+
+wildlife()
+
+elevation()
+
+
+function ColoradoStatePark() {
   howManyParks(); // => "42 parks!"
 
   return <h1>Colorado State Parks!</h1>;
+}
+
+ColoradoStatePark()
+
+
+export default function ColoradoStateParks() {
+ 
+  return (
+
+    <div>
+      <ColoradoStatePark />
+      <MesaVerde />
+    </div>
+  );
 }
